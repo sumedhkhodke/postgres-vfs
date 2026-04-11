a66460989417e4f85bda451ef11a7c5f66ae7c23
sumedhkhodke <sumedh.khodke@gmail.com>
2026-04-11 16:03:07 -0400

    simple revert of code diff leak

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { escapeLike } from "./fs/path-utils.js";
-
 /** Extract a message string from an unknown error */
 export function errorMessage(err: unknown): string {
   return err instanceof Error ? err.message : String(err);
@@ -8,15 +6,4 @@ export function errorMessage(err: unknown): string {
 /** Format a number[] as a pgvector literal string '[0.1,0.2,...]' */
 export function toVectorLiteral(embedding: number[]): string {
   return `[${embedding.join(",")}]`;
-}
-
-/** Truncate a string to a max length */
-export function truncate(str: string, maxLen: number): string {
-  if (str.length <= maxLen) return str;
-  return str.slice(0, maxLen) + "...";
-}
-
-/** Build a SQL LIKE pattern from user input, escaping wildcards */
-export function buildSearchPattern(query: string): string {
-  return `%${escapeLike(query)}%`;
 }
